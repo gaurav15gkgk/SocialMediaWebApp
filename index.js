665458d5b0457c0d79575e2edd654381bff8ffb5
gaurav15gkgk <72983652+gaurav15gkgk@users.noreply.github.com>
2021-03-27 20:28:37 +0530

    MongoDB atlas database connected

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /* Requiring Dependencies */
 const express = require('express')
 const morgan = require('morgan')
+const mongooose = require('mongoose')
+
+require('dotenv').config()
 
 
 const postRoute = require('./routes/post')
@@ -9,10 +12,22 @@ const app = express()
 
 app.use(morgan("dev"))
 
-app.use('/', postRoute)
+app.use('/', postRoute);
 
 
+//Connecting Database MongoDB
+mongooose
+    .connect(
+      process.env.DB_KEY_URL
+        ,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() =>{
+        console.log('MongoDB Connected')
+      
+    })
+    .catch(err => console.log(err))
 
-app.listen(8080, () => {
+const port = process.env.PORT || 8080
+
+app.listen(port , () => {
     console.log("Server started at http://localhost:8080")
 })
